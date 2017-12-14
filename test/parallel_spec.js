9640bb2865911e44d2f8ac0d16fcb8358b587486
Fábio D. Batista <fabio@elementarsistemas.com.br>
2017-12-14 19:52:19 -0200

    Reproducing test

---
+++ test/parallel_spec.js
@@ -0,0 +1,11 @@
+const parallel = require('mocha.parallel');
+
+describe('sample test', () => {
+  parallel('p', () => {
+    'a b c d e f g h'.split(' ').forEach(letter => {
+      it('should wait for ' + letter, () => {
+        return new Promise(resolve => setTimeout(resolve, 1000));
+			});
+		});
+	});
+});
