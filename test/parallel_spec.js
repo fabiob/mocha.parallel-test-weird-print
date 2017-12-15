ca2e5c955f1c6382d9dd125f5bf07f21d70f73e8
Fábio D. Batista <fabio@elementarsistemas.com.br>
2017-12-15 11:04:07 -0200

    Simplified test

--- test/parallel_spec.js
+++ test/parallel_spec.js
@@ -1,11 +1,10 @@
 const parallel = require('mocha.parallel');
 
 describe('sample test', () => {
-  parallel('p', () => {
-    'a b c d e f g h'.split(' ').forEach(letter => {
-      it('should wait for ' + letter, () => {
-        return new Promise(resolve => setTimeout(resolve, 1000));
-      });
-    });
+  describe('without mocha.parallel', () => {
+    it('should pass', () => {});
+  });
+  parallel('with mocha.parallel', () => {
+    it('should pass', () => {});
   });
 });
